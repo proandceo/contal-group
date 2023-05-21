28808b3847d3506b9e5cf1942bdbc576ea7303ef
Rex <proandceo@gmail.com>
2023-05-21 23:52:23 +1000

    create HomeController and index.html

--- src/main/java/com/contal/group/controller/HomeController.java
+++ src/main/java/com/contal/group/controller/HomeController.java
@@ -1,15 +1,13 @@
 package com.contal.group.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/")
+@Controller
 public class HomeController {
 
-    @GetMapping("")
-    public String home(){
-        return "hello world";
+    @GetMapping("/")
+    public String test(){
+        return "index";
     }
 }
