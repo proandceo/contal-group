4a26e428d9e0b7699f209c303760dfbcf79d5cdf
Rex <proandceo@gmail.com>
2023-05-21 16:36:19 +1000

    .

--- src/test/java/com/contal/group/GroupApplicationTests.java
+++ src/test/java/com/contal/group/GroupApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GroupApplicationTests {
+class roupApplicationTests {
 
 	@Test
 	void contextLoads() {
